c66ca3632a4450a299457993d24064def9a20527
John Loane <john.loane@dkit.ie>
2021-10-06 13:39:28 +0100

    Code to send email - pip install flask_mail
    Test this at home

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 import os
 
+import os
+import re
+
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+
+mail = Mail(app)
+
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-mysql_env = os.getenv('MYSQL_USER')
-print(mysql_env)
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sd3b_elective'
 
@@ -30,17 +45,27 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
+    email = request.form.get("email")
+    print(email)
     elective = request.form.get("elective")
-    if not name or not elective or elective not in ELECTIVES:
+    if not email or not elective or elective not in ELECTIVES:
         return render_template("failure.html")
-    STUDENTS_CHOICES[name]=elective
+    #STUDENTS_CHOICES[email]=elective
+    # cur = mysql.connection.cursor()
+    # cur.execute("INSERT INTO students(name, elective) VALUES (%s, %s)", (email, elective))
+    # mysql.connection.commit()
+    # cur.close()
+    message = Message("You are registered", recipients=[email])
+    mail.send(message)
     return redirect("/registrants")
 
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=STUDENTS_CHOICES)
+    cur = mysql.connection.cursor()
+    cur.execute("select name, elective from students")
+    registrants = cur.fetchall()
+    return render_template("registrants.html", registrants=registrants)
 
 
 app.run(debug=True)
